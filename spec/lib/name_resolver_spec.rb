51a081472ef98ea42832ac40efc7a5b3f52b3597
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-11-10 23:33:12 -0600

    several names test, pagination test

--- spec/lib/name_resolver_spec.rb
+++ spec/lib/name_resolver_spec.rb
@@ -7,15 +7,35 @@ describe GnresolverClient::NameResolvers do
         :get, '{"value":"Hyphessobrycon panamensis daguae", "localId": 1}'
       )
       expect(res).to be_kind_of Array
-      expect(res.size).to be > 0
-      expect(res.first).to be_kind_of Hash
       expect(res.first.keys.sort).
         to eq %i(localId matches page perPage suppliedNameString total)
+      expect(res.first[:matches].size).to be > 1
       expect(res.first[:matches].first.keys.sort).
         to eq %i(canonicalName canonicalNameUuid classificationPath
                  classificationPathIds classificationPathRanks dataSourceId
                  dataSourceTitle globalId kind nameString nameStringUuid
                  surrogate taxonId vernacular)
+    end
+
+    it "finds results for several names" do
+      res = subject.search(
+        :get, '[{"value":"Hyphessobrycon panamensis daguae", "localId": 1},
+                {"value": "Aegilops", "localId": 2}]'
+      )
+      expect(res.size).to be 2
+      expect(res.last[:total]).to be 3
+      expect(res.last[:matches].size).to be 3
+      expect(res.last.keys.sort).
+        to eq %i(localId matches page perPage suppliedNameString total)
+    end
+  end
+
+  context "pagination" do
+    it "returns pagination as a global parameter" do
+      # TODO: Fix #102
+      # Currently page and perPage show on the same level as total and
+      # localId/suppliedId, but it should come on the root-based "parameters"
+      # sections or something like that
     end
   end
 end
